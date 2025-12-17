4ed63f0625512ccaec7a55764fdce2e116dfa8ee
Bll4ck <64915679+Bll4ck@users.noreply.github.com>
2025-12-17 16:37:03 +0700

    ex 10 done

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,4 +1,20 @@
-const leapYears = function() {
+const leapYears = function(year) 
+{
+    const yearByFour = year / 4
+    const yearByHundred = year / 100
+    const yearByFourHundred = year / 400
+
+    let isLeapYear = false;
+    
+    if((yearByFour == Math.floor(yearByFour)) && (!(yearByHundred == Math.floor(yearByHundred)) || (yearByFourHundred == Math.floor(yearByFourHundred))))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+
 
 };
 
