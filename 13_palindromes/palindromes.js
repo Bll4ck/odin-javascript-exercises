6af50a62f9feb191e7cc8d0a3ae0fd2245454eda
Bll4ck <64915679+Bll4ck@users.noreply.github.com>
2025-12-18 20:17:48 +0700

    13 and 14 done

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,4 +1,27 @@
-const palindromes = function () {
+const palindromes = function (input) {
+    input = input.toLowerCase();
+    input = input.replaceAll(" ","");
+    input = input.replaceAll(".","");
+    input = input.replaceAll("!","");
+    input = input.replaceAll("?","");
+    input = input.replaceAll(",","");
+
+
+    let reverseInput = "";
+
+    for(i=input.length-1;i>=0;i--)
+    {
+        reverseInput += input[i]; 
+    }
+
+    if(input == reverseInput)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 
 };
 
--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
 
+    if (index < 0) return "OOPS";
+    if (index == 0) return 0;
+
+    let fibArray = [1,1];
+
+    if(index>2)
+    {
+        for(let i = 2;i < index;i++)
+        {
+            fibArray[i] = fibArray[i-2] + fibArray[i-1];
+        }
+    }
+
+    return fibArray[index-1];
+    
 };
 
 // Do not edit below this line
